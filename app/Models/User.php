04223501de1580e2b1a6a7e26903e781ef2f7d62
[Sloth] <[1183820@qq.com]>
2020-06-16 11:32:36 +0000

    集成 laravel-permission 扩展包

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
@@ -10,6 +11,8 @@ use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
+    use HasRoles;
+
     use MustVerifyEmailTrait;
 
     use Notifiable {
