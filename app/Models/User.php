5df2952c636dfaded61af528fea3bef6956ed4e4
[Sloth] <[1183820@qq.com]>
2020-06-21 06:03:57 +0000

    使用 JWT

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Traits\HasRoles;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 
-class User extends Authenticatable implements MustVerifyEmailContract
+class User extends Authenticatable implements MustVerifyEmailContract, JWTSubject
 {
     use Traits\LastActivedAtHelper;
 
@@ -110,5 +111,15 @@ class User extends Authenticatable implements MustVerifyEmailContract
         }
 
         $this->attributes['avatar'] = $path;
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -31,8 +31,17 @@ Route::prefix('v1')
                     ->name('users.store');
                 // 第三方登录
                 Route::post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
-                    ->where('social_type', 'weixin')
-                    ->name('socials.authorizations.store');
+                   ->where('social_type', 'weixin')
+                   ->name('api.socials.authorizations.store');
+                // 登录
+                Route::post('authorizations', 'AuthorizationsController@store')
+                   ->name('api.authorizations.store');
+                // 刷新token
+                Route::put('authorizations/current', 'AuthorizationsController@update')
+                   ->name('authorizations.update');
+                // 删除token
+                Route::delete('authorizations/current', 'AuthorizationsController@destroy')
+                   ->name('authorizations.destroy');
             });
 
         Route::middleware('throttle:' . config('api.rate_limits.access'))
